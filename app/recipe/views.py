ac7df3e771312733db1f3fef09207d98da01575d
naimishverma17 <itakgec@gmail.com>
2020-04-05 19:27:22 +0530

    refractoring

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,22 +6,26 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
-    serializer_class = serializers.TagSerializer
+class BaseAttrViewSet(viewsets.GenericViewSet,
+                      mixins.CreateModelMixin,
+                      mixins.ListModelMixin):
+    """View set for user designed attributes for ingredients"""
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
     authentication_classes = (TokenAuthentication,)
 
     def get_queryset(self):
-        """Return object of current authenticated user only"""
+        """User user specific objects"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new tag"""
+        """Create object"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseAttrViewSet):
+    """Manage tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
@@ -29,14 +33,4 @@ class IngredientViewSet(viewsets.GenericViewSet,
                         mixins.CreateModelMixin):
     """Manage ingredient in the database"""
     serializer_class = serializers.IngredientSerializer
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
-    authentication_classes = (TokenAuthentication,)
-
-    def get_queryset(self):
-        """Return object of current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new ingredient"""
-        serializer.save(user=self.request.user)
