27df6e6782b9f1751b38fa5a8b4111563d155410
naimishverma17 <itakgec@gmail.com>
2020-04-06 23:53:56 +0530

    flake 8 fix

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls')),
-    path('api/recipe/', include('recipe.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('api/user/', include('user.urls')),
+                  path('api/recipe/', include('recipe.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
