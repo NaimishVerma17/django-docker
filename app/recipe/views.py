55543415b5493042fcb8a124cf6f6aab46d7c879
naimishverma17 <itakgec@gmail.com>
2020-04-08 22:20:22 +0530

    added filter for asigned tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,7 +17,15 @@ class BaseAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """User user specific objects"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create object"""
